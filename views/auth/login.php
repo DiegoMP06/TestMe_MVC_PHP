9c68d371f120ac0a70dbeee490efd94e7000efc9
Diego Meneses <perezdi060606@gmail.com>
2023-09-23 23:14:50 -0600

    Vistas de login Version 1.0

--- views/auth/login.php
+++ views/auth/login.php
@@ -3,16 +3,16 @@
     <p class="descripcion-pagina">Ya Tienes una Cuenta Inicia Sesion</p>
 
     <form action="/" class="formulario" method="POST">
-        <?php alertasFormulario("email", $alertas) ?>
         <div class="campo">
             <label for="email">Correo Electronico: </label>
             <input type="email" name="email" id="email" placeholder="Tu Correo Electronico">
+            <?php alertasFormulario("email", $alertas) ?>
         </div>
 
-        <?php alertasFormulario("email", $alertas) ?>
         <div class="campo">
             <label for="password">Contraseña: </label>
             <input type="password" name="password" id="password" placeholder="Tu Contraseña">
+            <?php alertasFormulario("email", $alertas) ?>
         </div>
 
         <input type="submit" value="Iniciar Sesión" class="boton-azul-block">
